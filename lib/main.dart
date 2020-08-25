f1cf9d1b0740f57c0b73a4f296da6db749607012
Douglas Iacovelli <douglas.iacovelli@gmail.com>
2020-08-25 19:35:49 -0300

    doug

--- lib/main.dart
+++ lib/main.dart
@@ -32,42 +32,32 @@ class MyHomePage extends StatelessWidget {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         children: <Widget>[
-          Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(padding: EdgeInsets.all(10.0)),
-              Flex(
-                direction: Axis.horizontal,
-                children: <Widget>[
-                  Text(
-                    "Texto titulo",
-                    style: TextStyle(fontSize: 30.0),
-                  ),
-                  Text(
-                    "Text descritivo",
-                    style: TextStyle(fontSize: 20.0),
-                  )
-                ],
-              ),
-              Column(children: <Widget>[
-                Container(
-                  color: Colors.red[100],
+          Padding(padding: EdgeInsets.all(10.0)),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Flex(
+              direction: Axis.vertical,
+              children: <Widget>[
+                Text(
+                  "Texto titulo",
+                  style: TextStyle(fontSize: 30.0),
+                ),
+                Text(
+                  "Text descritivo",
+                  style: TextStyle(fontSize: 20.0),
                 )
-              ])
+              ],
+            ),
+          ),
+          Column(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  color: Colors.red[100],
+                  child: Text("teste"),
+                ),
+              )
             ],
           )
         ],
